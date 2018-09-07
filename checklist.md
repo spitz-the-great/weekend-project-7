074781735ab84c182613c0903355778d593fe15d
spitz-the-great <jspitzley85@gmail.com>
2018-09-07 16:15:24 -0500

    npm install react, logger, router

---
+++ checklist.md
@@ -0,0 +1,6 @@
+## Initial Setup
+[ ] make folder structure
+[ ] ```npm install react-router-dom, react-logger```
+[ ] bring in router, logger
+[ ] set up store
+[ ] set up routes
--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+
+}
+
+const global = ( state = defaultState, action) =>{
+    return state;
+}
+
+
+
+const storeInstance = createStore(
+    combineReducers({global}),
+    applyMiddleware(logger),
+);
+
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
