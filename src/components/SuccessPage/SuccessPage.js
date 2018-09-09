59fd6b66cd51740b3f174986a7511f6f7c4007b2
spitz-the-great <jspitzley85@gmail.com>
2018-09-09 13:34:38 -0500

    success page redirect to home and clear inputs working

--- src/components/SuccessPage/SuccessPage.js
+++ src/components/SuccessPage/SuccessPage.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 
 class SuccessPage extends Component {
 
+    handleClick = () =>{
+        this.props.history.push('/');
+    }
+
+
     render() {
 
         return (
             <div>
-                success!
+                <h2>Thanks for that. I appreciate you.</h2>
+                <button onClick={this.handleClick}>Leave New Feedback</button>
         </div>
 
         )
